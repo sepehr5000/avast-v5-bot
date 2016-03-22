f8936f5a5984f353b03c87aae2c56f2492e49e8c
avast team! <titanteam0098@gmail.com>
2016-03-22 15:42:11 +0430

    Update autolaunch.lua
    Edited!

--- autolaunch.lua
+++ autolaunch.lua
@@ -1,1 +1,1 @@
-description "superflux upstart script" respawnrespawn limit 15 5 start on runlevel [2345]stop on shutdown setuid yourusernameexec /bin/sh telegrambotpath/launh.sh
+description "superflux upstart script" respawnrespawn limit 15 5 start on runlevel [2345]stop on shutdown setuid yourusernameexec /bin/sh telegrambotpath/launch.sh
