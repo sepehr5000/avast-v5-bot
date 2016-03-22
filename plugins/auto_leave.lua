59751d6e9f1ed36f9990b404431d252e2c3732e2
avast team! <titanteam0098@gmail.com>
2016-03-23 02:21:57 +0430

    Create auto_leave.lua

---
+++ plugins/auto_leave.lua
@@ -0,0 +1,23 @@
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg)
+  if msg.service and msg.action.type == 'chat_add_user' then
+    local data = load_data(_config.moderation.data)
+    if not data[tostring(msg.to.id)] then
+      print "This is not our group. Leaving..."
+      chat_del_user('chat#id'..msg.to.id, 'user#id'..167946378, callback, false)
+    end
+  end
+end
+
+return {
+  description = "Kicking ourself (bot) from unmanaged groups.",
+  usage = "Plugin For Kiling add with id ....",
+  patterns = {
+    "^!!tgservice (.+)$"
+  },
+  run = run
+}
